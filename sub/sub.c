321e2dd8c84cadb81695b504f71198952fe1ecf6
venkatb138 <venkataramanb138@gmail.com>
2023-04-12 20:28:06 +0000

    Adding the file sub.c

---
+++ sub/sub.c
@@ -0,0 +1,37 @@
+// code for substraction of two numbers
+
+#include <stdio.h>
+
+int main()
+
+{
+
+        // declaration of variables
+
+        int i4num1 ;
+
+        int i4num2 ;
+
+        int i4resultsub ;
+
+        // scanning the values from the user
+
+        printf("Enter the num1:") ;
+
+        scanf("%d", &i4num1) ;
+
+        printf("Enter the num2:") ;
+
+        scanf("%d", &i4num2) ;
+
+        // main logic
+
+        i4resultsub = i4num1 - i4num2 ;
+
+
+        // printing the output
+
+        printf("Substraction of given 2 numbers is: %d\n", i4resultsub) ;
+
+}
+
