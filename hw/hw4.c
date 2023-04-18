541f3775570544fe6c0f98cda400741198696a5b
venkatb138 <venkataramanb138@gmail.com>
2023-04-18 07:41:17 +0000

    adding the file hw4.c

---
+++ hw/hw4.c
@@ -0,0 +1,30 @@
+//hw4.c
+
+#include <stdio.h>
+
+int main()
+{
+	// declaaration of variables
+	
+	int a ;
+	int b = 10 ;
+
+	// logic
+	
+	a = b + 10 * 2 - 5 / 3 ;
+
+	// printing the output
+	
+	printf("%d\n", a) ;
+
+	// exit 
+	
+	return 0 ;
+
+}
+
+
+
+
+
+
