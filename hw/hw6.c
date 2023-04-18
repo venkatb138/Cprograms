4eec66ee698227b65a854594360009f1ad2ba612
venkatb138 <venkataramanb138@gmail.com>
2023-04-18 08:08:07 +0000

    adding the file hw6.c

---
+++ hw/hw6.c
@@ -0,0 +1,36 @@
+// hw6.c
+
+#include <stdio.h>
+
+int main()
+{
+	//declaration of variable
+	
+	int a = 4294967295 ;
+
+	int b ;
+
+	// logic
+	
+	b= a+1 ;
+
+	// printing the output
+	
+
+        printf("%d",b) ;
+
+	// exit
+	
+	return 0 ;
+
+}
+
+
+
+/*
+ *
+ *
+ * output: 0 
+ *
+ *
+ */
