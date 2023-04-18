36c92c80083300377567f82ae2d8da527b5e413c
venkatb138 <venkataramanb138@gmail.com>
2023-04-18 08:18:06 +0000

    adding the file hw7.c

---
+++ hw/hw7.c
@@ -0,0 +1,36 @@
+// hw6.c
+
+#include <stdio.h>
+
+int main()
+{
+	//declaration of variable
+	
+	int a = 2147483647 ;
+
+	int b ;
+
+	// logic
+	
+	b= a+1 ;
+
+	// printing the output
+	
+
+        printf("%d",b) ;
+
+	// exit
+	
+	return 0 ;
+
+}
+
+
+
+/*
+ *
+ *
+ * output: -2147483648
+ *
+ *
+ */
