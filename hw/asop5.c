a66209b1560f9e15d8f798dbbe61b15a88dfba69
venkatb138 <venkataramanb138@gmail.com>
2023-04-18 21:53:34 +0000

    adding the files asop2.c asop3.c asop4.c asop5.c

---
+++ hw/asop5.c
@@ -0,0 +1,36 @@
+//assignment oprators observation 1
+
+#include <stdio.h>
+
+int main()
+{
+	//variable declaration
+	 
+	int m = 5 ;
+
+	
+       	//logic
+	
+	m %= 5 ; 
+
+
+	// printing the output
+
+	printf("%d", m) ;
+
+
+
+	//exit
+	
+	return 0 ;
+
+}
+
+
+/*
+ *
+ *
+ * output: 0
+ *
+ *
+ */
