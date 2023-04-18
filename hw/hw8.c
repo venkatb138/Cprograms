672ab33577a1604f313a2e65d709872b5f935adf
venkatb138 <venkataramanb138@gmail.com>
2023-04-18 08:23:06 +0000

    adding the file hw8.c

---
+++ hw/hw8.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main()
+
+{
+	int n = 0 ;
+
+	printf(" %d " , n ) ;
+
+	printf(" %c ", n ) ;
+	
+	printf(" %x ", n ) ;
+
+	printf(" %o ", n ) ;
+
+	return 0 ;
+}
+
+
+
+/*
+ *
+ * output: 0 _  0 0 
+ *
+ *
+ */
+
