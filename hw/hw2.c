4aae21a8a4c8fb387358145be505ff161b9edde6
venkatb138 <venkataramanb138@gmail.com>
2023-04-18 07:03:20 +0000

    adding the file hw2.

---
+++ hw/hw2.c
@@ -0,0 +1,35 @@
+// hw2.c file
+
+#include <stdio.h>
+
+int main()
+{
+	// declaration of variable
+	
+	float a ;
+
+	int b , c ;
+
+	// assign the values
+
+	 b = 8 ;
+
+	 c = 3 ;
+
+	// logic
+
+	a = b%c ;
+
+	// printing the output
+	
+	printf(" %f ", a) ;
+
+}
+
+
+
+
+/*
+ *
+ *
+ * output: 2.000000
