16555467e81f88afc2472b44ed3cd5b5c145cc9c
venkatb138 <venkataramanb138@gmail.com>
2023-04-18 21:50:37 +0000

    adding the file asop.c

---
+++ hw/asop2.c
@@ -0,0 +1,29 @@
+//assignment oprators observation 1
+
+#include <stdio.h>
+
+int main()
+{
+	//variable declaration
+	 
+	int m = 5 ;
+
+	//printing the output with logic
+
+	printf("%d\t", m -= 5.0) ;
+	
+  
+	//exit
+	
+	return 0 ;
+
+}
+
+
+/*
+ *
+ *
+ * output: 0
+ *
+ *
+ */
