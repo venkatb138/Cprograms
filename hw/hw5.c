60e49a0dfaaa5d2da171061d680260c4ff292b78
venkatb138 <venkataramanb138@gmail.com>
2023-04-18 07:57:41 +0000

    adding the file hw5.c

---
+++ hw/hw5.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main()
+
+{
+	int n = -1 ;
+
+	printf(" %d " , n ) ;
+
+	printf(" %c ", n ) ;
+	
+	printf(" %x ", n ) ;
+
+	printf(" %o ", n ) ;
+
+	return 0 ;
+}
+
+
+
+/*
+ *
+ * output: 1234567 -  12d687 4553207
+ *
+ *
+ */
+
