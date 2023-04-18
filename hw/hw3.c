a4945dbff4ebadd17530e669a2f55ba1fbdf9442
venkatb138 <venkataramanb138@gmail.com>
2023-04-18 07:26:44 +0000

    adding the file hw3.c

---
+++ hw/hw3.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+
+{
+	char c = 'R' ;
+
+	printf(" %d " , c ) ;
+
+	printf(" %c ", c ) ;
+	
+	printf(" %x ", c ) ;
+
+	printf(" %o ", c ) ;
+
+	return 0 ;
+}
+
+
+
+
