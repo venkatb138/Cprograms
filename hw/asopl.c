b77736de9abba0094cff5a1410f27855f17cc6fc
venkatb138 <venkataramanb138@gmail.com>
2023-04-19 03:29:10 +0000

    adding the file asopl.c asopl1.c asopl2.c asopl3.c asopl4.c

---
+++ hw/asopl.c
@@ -0,0 +1,25 @@
+// arithmatic operations with char 'c'
+
+
+#include<stdio.h>
+
+int main()
+{
+	float f =10.0 ;
+
+	f += 'c' ;
+
+	printf("%f",f) ;
+
+	// exit
+	
+	return 0 ;
+
+}
+
+
+
+/*
+ *
+ * output: 109.000000
+ */
---
+++ hw/asopl3.c
@@ -0,0 +1,30 @@
+// with char 'c'
+
+#include <stdio.h>
+
+int main()
+{
+	// variable declaration
+	
+	float f = 10.0 ;
+
+	// logic
+	
+	f /= 'c' ;
+
+	// printing the output
+	
+	printf("%f",f) ;
+
+	// exit
+	
+	return 0 ;
+
+}
+
+
+/*
+ *output: 0.101010
+ *
+ */
+
