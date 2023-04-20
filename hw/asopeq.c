85add31895b76120c4a68b7948bff563c53ff146
venkatb138 <venkataramanb138@gmail.com>
2023-04-20 19:58:04 +0000

    adding the file asopq.c

---
+++ hw/asopeq.c
@@ -0,0 +1,30 @@
+// hw with assign operator
+
+#include <stdio.h>
+
+int main()
+
+{
+	// variable declaration
+	
+	int a, b ,r ;
+
+	// assign values
+	 a = 10 ;
+
+	 b = 20 ;
+
+	 // logic
+
+	 r = a^=b^=a^=b ;
+
+	 // print the output 
+
+	 printf("%d", r) ;
+
+	 // exit
+	 
+	 return 0 ;
+
+}
+
